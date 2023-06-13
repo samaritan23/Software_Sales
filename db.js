fd000a9f1e7d83680ff2ad0b985102e035f83246
samaritan23 <bakshitamoghno6@gmail.com>
2023-06-14 04:05:11 +0530

    Final Commit

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 const mongoUri = "mongodb://localhost:27017/Sales_DB?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
-const connectToMongo = () => {
-    mongoose.connect(mongoUri)
+const connectToMongo = async () => {
+    await mongoose.connect(mongoUri)
 }
 
 module.exports = connectToMongo;
