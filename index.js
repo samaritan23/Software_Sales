f49c7f9de8c131d8e64d9d032ea155b4f34140e7
samaritan23 <bakshitamoghno6@gmail.com>
2023-06-14 04:04:41 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const connectToMongo = require("./db")
 const express = require('express')
+const bp = require('body-parser')
 var cors = require('cors')
 require("dotenv").config()
 
-connectToMongo();
+connectToMongo().then(() => console.log('Connected to MongoDB successfully'))
+                .catch(err => console.log(err));
+
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
+app.use(bp.urlencoded({ extended: true }))
 app.use(cors())
 
 // This will route the request to ./routes/sales
